8f73f796c44fbbbac9545d1bcff60c98834eb861
vientoFuerte <49725325+vientoFuerte@users.noreply.github.com>
2026-04-12 16:56:21 +0300

    Update database.h
    Об.явление функций бд

--- server/database.h
+++ server/database.h
@@ -1,1 +1,25 @@
+#pragma once
 
+#include <memory>
+#include <iostream>
+#include <sqlite3.h>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
+
+struct clientInfo{
+   std::shared_ptr<tcp::socket> socket;
+   std::string username;
+};
+
+
+
+// Хранилище всех подключённых клиентов (сокеты)
+extern std::vector<clientInfo> clients;
+extern std::mutex clients_mutex;
+
+
+int database_init(sqlite3* db);
+
+// Функция для обработки одного клиента
+void handle_client(std::shared_ptr<tcp::socket> socket) ;
