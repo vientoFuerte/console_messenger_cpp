6cab78ef42e00253ae8c1758ec549c10e60c2802
vientoFuerte <49725325+vientoFuerte@users.noreply.github.com>
2026-04-14 17:30:22 +0300

    Update client.h
    Заготовка класса для клиента.

--- client/client.h
+++ client/client.h
@@ -1,1 +1,16 @@
+#pragma once
 
+#include <boost/asio.hpp>
+#include <string>
+
+class Client {
+public:
+    Client(const std::string& host, int port);
+    void Run();           // основной цикл
+    ~Client();
+
+private:
+    void SendMessages();
+    void ReceiveMessages();
+  
+};
