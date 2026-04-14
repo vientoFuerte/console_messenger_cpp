ae80f1728e72c7a1527c06656bec8f10fb29c0c3
vientoFuerte <49725325+vientoFuerte@users.noreply.github.com>
2026-04-14 18:55:47 +0300

    Update client.cpp
    Заготовка для реализации клиента.

--- client/client.cpp
+++ client/client.cpp
@@ -1,1 +1,31 @@
+#include <client.h>
 
+class Client {
+public:
+    Client(const std::string& host, int port);
+    void Run();           // основной цикл
+    ~Client();
+
+private:
+    std::string host_;
+    int port_;
+    void SendMessages();
+    void ReceiveMessages();
+
+};
+
+Client::Client (const std::string& host, int port)
+{
+  host_=host;
+  port_=port;
+}
+
+
+Client::void SendMessages()
+{
+  
+}
+Client::void ReceiveMessages()
+{
+  
+}
