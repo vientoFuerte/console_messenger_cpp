9e772b7040efaca8e4683fd98d71016fb3cde0e5
vientoFuerte <49725325+vientoFuerte@users.noreply.github.com>
2026-04-20 15:37:52 +0300

    Update client.h
    Корректировка заголовочного файла для клиента.

--- client/client.h
+++ client/client.h
@@ -1,7 +1,11 @@
 #pragma once
 
 #include <boost/asio.hpp>
+#include <iostream>
 #include <string>
+#include <thread>
+
+using boost::asio::ip::tcp;
 
 class Client {
 public:
@@ -10,7 +14,12 @@ public:
     ~Client();
 
 private:
-    void SendMessages();
+    boost::asio::io_context io_context_;
+    std::unique_ptr<tcp::socket> socket_;
+    std::string name_;
+
+    
+    void SendMessages(tcp::socket& socket);  // Функция для отправки сообщений в отдельном потоке
     void ReceiveMessages();
   
 };
