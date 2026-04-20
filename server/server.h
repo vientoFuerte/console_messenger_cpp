014165d8e6875bda875860db607d6e357cb597ca
vientoFuerte <49725325+vientoFuerte@users.noreply.github.com>
2026-04-20 23:45:33 +0300

    Update server.h
    Убраны заголовки функций, относящихся к БД.

--- server/server.h
+++ server/server.h
@@ -8,12 +8,5 @@
 
 void server_run(int port);
 
-// Функция для выполнения SQL-запросов
-bool execute_sql(const std::string& sql);
 
-// Сохранение сообщения в базу данных
-void save_message(const std::string& from_user, const std::string& to_user, const std::string& message, bool is_private, sqlite3* dbs);
-
-// Загрузка последних сообщений
-void load_messages(const std::string& username, std::shared_ptr<tcp::socket> socket, sqlite3* db, int limit = 5);
 
