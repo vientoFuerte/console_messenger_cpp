8b8720f799aa17dd0155832eda3cc98332272abe
vientoFuerte <49725325+vientoFuerte@users.noreply.github.com>
2026-04-20 23:46:46 +0300

    Update database.h
    Добавлены заголовки переносимых в этот модуль функций.

--- server/database.h
+++ server/database.h
@@ -22,4 +22,10 @@ extern std::mutex clients_mutex;
 int database_init(sqlite3* db);
 
 // Функция для обработки одного клиента
-void handle_client(std::shared_ptr<tcp::socket> socket) ;
+void handle_client(std::shared_ptr<tcp::socket> socket);
+
+// Сохранение сообщения в базу данных
+void save_message(const std::string& from_user, const std::string& to_user, const std::string& message, bool is_private, sqlite3* db);
+
+// Загрузка последних сообщений
+void load_messages(const std::string& username, std::shared_ptr<tcp::socket> socket, sqlite3* db, int limit = 5);
