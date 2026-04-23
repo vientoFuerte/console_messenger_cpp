661465906327a3e06fdae67d54603050a7417f78
vientoFuerte <49725325+vientoFuerte@users.noreply.github.com>
2026-04-23 15:54:19 +0300

    Update database.h
    Красивое оформление с комментариями.

--- server/database.h
+++ server/database.h
@@ -7,25 +7,22 @@
 
 using boost::asio::ip::tcp;
 
-struct clientInfo{
-   std::shared_ptr<tcp::socket> socket;
-   std::string username;
+/**
+ * @brief Структура для хранения информации о подключённом клиенте
+ */
+struct clientInfo {
+    std::shared_ptr<tcp::socket> socket;  // Умный указатель на сокет клиента
+    std::string username;                  // Имя пользователя
 };
 
+// Глобальные переменные для управления клиентами
+extern std::vector<clientInfo> clients;      // Вектор всех подключённых клиентов
+extern std::mutex clients_mutex;             // Мьютекс для потокобезопасного доступа к clients
 
-
-// Хранилище всех подключённых клиентов (сокеты)
-extern std::vector<clientInfo> clients;
-extern std::mutex clients_mutex;
-
-
-sqlite3* database_init();
-
-// Функция для обработки одного клиента
-void handle_client(std::shared_ptr<tcp::socket> socket);
-
-// Сохранение сообщения в базу данных
-void save_message(const std::string& from_user, const std::string& to_user, const std::string& message, bool is_private, sqlite3* db);
-
-// Загрузка последних сообщений
-void load_messages(const std::string& username, std::shared_ptr<tcp::socket> socket, sqlite3* db, int limit = 5);
+// Функции для работы с базой данных
+sqlite3* database_init();                                           // Инициализация БД
+void handle_client(std::shared_ptr<tcp::socket> socket);           // Обработка клиента
+void save_message(const std::string& from_user, const std::string& to_user, 
+                  const std::string& message, bool is_private, sqlite3* db);  // Сохранение сообщения
+void load_messages(const std::string& username, std::shared_ptr<tcp::socket> socket, 
+                   sqlite3* db, int limit = 5);                    // Загрузка истории сообщений
