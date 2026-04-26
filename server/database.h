ab16753165f46c8c6895919f78493308536d53d0
vientoFuerte <49725325+vientoFuerte@users.noreply.github.com>
2026-04-26 18:29:38 +0300

    Update database.h
    Добавлена проверка команды выхода.

--- server/database.h
+++ server/database.h
@@ -20,9 +20,10 @@ extern std::vector<clientInfo> clients;      // Вектор всех подкл
 extern std::mutex clients_mutex;             // Мьютекс для потокобезопасного доступа к clients
 
 // Функции для работы с базой данных
-sqlite3* database_init();                                           // Инициализация БД
+sqlite3* database_init();                                          // Инициализация БД
 void handle_client(std::shared_ptr<tcp::socket> socket);           // Обработка клиента
 void save_message(const std::string& from_user, const std::string& to_user, 
                   const std::string& message, bool is_private, sqlite3* db);  // Сохранение сообщения
 void load_messages(const std::string& username, std::shared_ptr<tcp::socket> socket, 
                    sqlite3* db, int limit = 5);                    // Загрузка истории сообщений
+bool is_quit(const std::string& msg);                              // Проверка команды выхода
