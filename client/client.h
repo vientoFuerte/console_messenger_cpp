2eec6e57346e0f9f290efab2427a7c096506cc89
vientoFuerte <49725325+vientoFuerte@users.noreply.github.com>
2026-04-27 13:16:47 +0300

    Update client.h
    Добавлены комментарии.

--- client/client.h
+++ client/client.h
@@ -11,17 +11,28 @@ using boost::asio::ip::tcp;
 
 class Client {
 public:
+     /** @brief Конструктор клиента 
+     * @param host Адрес сервера (например, localhost)
+     * @param port Номер порта сервера
+     */
     Client(const std::string& host, int port);
+
+   /** @brief Запуск клиента */
     void Run();           // основной цикл
+
+    /** @brief Деструктор клиента 
+     */
     ~Client();
 
 private:
-    boost::asio::io_context io_context_;
-    std::unique_ptr<tcp::socket> socket_;
-    std::string name_;
+    boost::asio::io_context io_context_;     // Асинхронный контекст
+    std::unique_ptr<tcp::socket> socket_;    // TCP сокет
+    std::string name_;                       // Имя пользователя
 
-    
-    void SendMessages();  // Функция для отправки сообщений в отдельном потоке
+     /** @brief Функция для отправки сообщений пользователя (в отдельном потоке)*/
+    void SendMessages();
+
+    /** @brief Фукнция приема сообщений от сервера */
     void ReceiveMessages();
   
 };
